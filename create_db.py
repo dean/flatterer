7dc9b1b8c8657655c9704ec033b27ca57d55b123
Dean Johnson <deanjohnson222@gmail.com>
2014-12-08 22:31:18 -0800

    Added default admin account for dev instances.

--- create_db.py
+++ create_db.py
@@ -2,3 +2,8 @@ from flatterer import app, db
 from flatterer.models import User
 
 db.create_all()
+
+if app.config['DATABASE_TYPE'] == 'dev':
+    u = User('johnsdea', 'Dean', 'password', admin=True)
+    db.session.add(u)
+    db.session.commit()
